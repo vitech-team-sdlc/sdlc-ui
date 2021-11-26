aa47f83cba07ed0f329d3fff063a79bc76e5e30d
Yurii Martynenko <yurii.martynenko@vitechteam.com>
2021-11-26 12:07:45 +0200

    chore: add github action

---
+++ src/components/Aside.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <aside class="w-1/4 bg-grey float-left h-screen">
+      Content Aside
+    </aside>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Aside'
+})
+</script>
+
+<style scoped>
+</style>
